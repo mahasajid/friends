41740089b6011cf436ce8eef5f664e28559f3b62
Maha Sajid <mahasajid1@gmail.com>
2022-06-21 18:44:17 +0500

    db changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   root 'home#index'
   get 'home/about'
+  get '/articles', to: 'articles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
